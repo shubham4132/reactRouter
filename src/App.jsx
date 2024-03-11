b26b49751b81ee2fe9070bc76b253a6be58de908
shubham4132 <shubhamg3122134@gmail.com>
2024-03-11 18:22:22 +0530

    Learning react-Router and builded one Project on these

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 
 function App() {
   //const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="bg-green-600 p-4">React Router</h1>
+      {/* <Header /> */}
+      {/* <Home /> */}
+      {/* <Footer /> */}
     </>
   );
 }
